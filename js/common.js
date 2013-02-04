06184f6578341e2143ad8cc6f74d85448c064d7e
Yevgen <godun.yevgen@gmail.com>
2013-02-04 17:27:25 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
 
+    // progress
+    // $(".meter > span").each(function() {
+    //     $(this)
+    //         .data("origWidth", $(this).width())
+    //         .width(0)
+    //         .animate({
+    //             width: $(this).data("origWidth")
+    //         }, 1200);
+
+    $(".header__download .counter").click(function(){
+        if ($(this).hasClass("js-active")) {
+            $(this).parent().removeClass("active");
+            $(this).removeClass("js-active");
+            $(this).next().slideUp("fast");
+        }
+        else {
+            $(this).parent().addClass("active");
+            $(this).addClass("js-active");
+            $(this).next().slideDown("fast");
+        }
+    });
+
+
 });
