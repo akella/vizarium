7d6acd63633a8c93e55d31fcd88d2f573913541c
Yevgen <godun.yevgen@gmail.com>
2013-02-11 17:55:06 +0200

    fixes (part1)

--- js/common.js
+++ js/common.js
@@ -41,9 +41,6 @@ $(document).ready(function() {
             $(".js-btn-2").addClass("active");
             $('.js-films-2').addClass('film-wrap_act');
             $(".js-filter-2").addClass('filter-wrap_act');
-            $('#js-nav-2 .shadow').animate({
-                opacity: 1,
-              }, 500 );
         }
         else {
             $('#js-nav-2').removeClass('nav-wrap_act');
@@ -78,22 +75,27 @@ $(document).ready(function() {
                 "top": 60,
             });
         }
-
-        // --- animate shadow ---
-        if (($('body').scrollTop() > 10)) {
-            $('#js-nav-1 .shadow').animate({
-                opacity: 1,
-              }, 500 );
-        }
-        else {
-            $('#js-nav-1 .shadow').animate({
-                opacity: 0,
-              }, 500 );
-        }
-
         if ($('body').scrollTop() < 100) {
             $(".js-btn-1").addClass("active");
             $(".js-btn-2").removeClass("active");
+        }
+    });
+
+//-------------- show/hide shadow ------------------------------- //
+    $(window).scroll(function() {
+        // animate shadow 1        
+        if (($('body').scrollTop() > 30)) {
+            $('#js-nav-1 .shadow').fadeIn();
+        }
+        else {
+            $('#js-nav-1 .shadow').fadeOut();
+        }
+        // animate shadow 2
+        if ($('body').scrollTop() >= (position_nav_2 - 48)) {
+            $('#js-nav-2 .shadow').fadeIn();
+        }
+        else {
+            $('#js-nav-2 .shadow').fadeOut();
         }
     });
 
@@ -205,25 +207,50 @@ $(document).ready(function() {
 
 //  ---------------- select film options ----------------------- //
     $(".select__wrap").click(function(){
-        if ($(this).hasClass("js-active")) {
-            $(this).removeClass("js-active");
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
             $(this).next().slideUp("fast");
         }
         else {
-            $(".select__wrap").removeClass("js-active");
-            $(this).addClass("js-active");
-            $(".select ul").slideUp("fast");
+            $(".select__wrap").removeClass("active");
+            $(this).addClass("active");
+            $("filter-extend").slideUp("fast");
+            $(this).next().slideDown("fast");
+        }
+    });
+//  ---------------- Extend list ----------------------- //
+    $(".js-extend").hide();
+    $(".filter-list__title").click(function(){
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            $(this).next().slideUp("fast");
+        }
+        else {
+            $(this).removeClass("active");
+            $(this).addClass("active");
+            $("filter-extend").slideUp("fast");
             $(this).next().slideDown("fast");
         }
     });
 
+//  ---------------- sidebar-height ----------------------- //
+    var filter_height = $(".js-filter-2").height();
+    var page_height = $("html").height();
+    // alert(filter_height);
+    // alert(page_height);
+    if (filter_height >= page_height) {
+        //alert("BIG");
+        $(".js-filter-2").addClass("filter-scroll");
+        $(".js-filter-2").css("height", page_height-100);
+    }
+
 //  ------------------------ selects ------------------------- //
-    $(".select li").click(function(){
-        var val = $(this).html();
-        $(this).parent().prev().children("span").html(val);
-        $(this).parent().slideUp("fast");
-        $(this).parent().prev().removeClass("js-active");
-    });
+    // $(".select li").click(function(){
+    //     var val = $(this).html();
+    //     $(this).parent().prev().children("span").html(val);
+    //     $(this).parent().slideUp("fast");
+    //     $(this).parent().prev().removeClass("js-active");
+    // });
 
 // -------------------- watch status buttons ----------------- //
     $(".watch-status button").click(function(){
